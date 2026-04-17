0aa1fc1dde9c49e6bce2ad1dd4dfb166bd1e0119
Nagita Aliya Sanopa <139783419+taaliyas@users.noreply.github.com>
2026-04-17 14:22:31 +0700

    Update sensor.c

--- sensor.c
+++ sensor.c
@@ -12,36 +12,37 @@ struct msg_buffer {
     char msg_text[200];
 };
 
-int q1, q2;
-int id;
-int running = 1;
+int id, pid, q1, q2;
+int running = 1, ex = 0;
 
 pthread_mutex_t lock;
 
-void* listener_thread(void* arg) {
+void* thread(void* arg) {
     struct msg_buffer msg;
 
     while (1) {
-        if (msgrcv(q2, &msg, sizeof(msg) - sizeof(long),
-                   getpid(), 0) != -1) {
+		msgrcv(q2, &msg, sizeof(msg) - sizeof(long), pid, 0);
+        pthread_mutex_lock(&lock);
 
-            pthread_mutex_lock(&lock);
+        if (strcmp(msg.msg_text, "SHUTDOWN") == 0) {
+			if(ex){
+				pthread_mutex_unlock(&lock);
+				break;
+			}
+			
+            printf("\n[INFO] Another sensor has exited\n");
+			printf("[INFO] Cancelling current input...\n");
+            printf("[INFO] System shutting down...\n");
 
-            if (strcmp(msg.msg_text, "SHUTDOWN") == 0) {
-                printf("\n[INFO] Another sensor has exited\n");
-                printf("[INFO] Cancelling current input...\n");
-                printf("[INFO] System shutting down...\n");
-
-                running = 0;
-                pthread_mutex_unlock(&lock);
-                break;
-            }
-
-            printf("\n[INFO] Current city status: %s\n\n", msg.msg_text);
-            printf("Masukkan data:\n\n");
-
+            running = 0;
             pthread_mutex_unlock(&lock);
+			break;
         }
+
+        printf("\n[INFO] Current city status: %s\n\n", msg.msg_text);
+        printf("Masukkan data:\n\n");
+
+        pthread_mutex_unlock(&lock);
     }
     return NULL;
 }
@@ -59,73 +60,64 @@ int main() {
 
     msg.msg_type = 1;
     msg.s_id = getpid();
+	pid = msg.s_id;
     strcpy(msg.msg_text, "REGISTER");
+	
     msgsnd(q1, &msg, sizeof(msg) - sizeof(long), 0);
-
     msgrcv(q2, &msg, sizeof(msg) - sizeof(long), getpid(), 0);
     id = msg.s_id;
 
     pthread_t tid;
-    pthread_create(&tid, NULL, listener_thread, NULL);
+    pthread_create(&tid, NULL, thread, NULL);
 
+	printf("Masukkan data:\n");
+	
     while (running) {
+		int vcount = 0;
         char d1[50], d2[50];
-        int vcount = 0;
-
-        pthread_mutex_lock(&lock);
-        printf("Masukkan data:\n\n");
-        pthread_mutex_unlock(&lock);
-
+		
         while (vcount < 2 && running) {
-            char line[100];
             int input;
-            char loc[10], stat[10];
+            char in[50], loc, stat;
 
-            scanf(" %[^\n]", line);
+            scanf(" %[^\n]", in);
 
-            if (!running) break;
-
-            if (strcmp(line, "exit") == 0) {
-                pthread_mutex_lock(&lock);
+            if (!running){
+				break;
+			}
+			
+            if (strcmp(in, "exit") == 0) {
                 printf("[SENSOR] Sending exit signal...\n");
-                pthread_mutex_unlock(&lock);
 
                 msg.msg_type = 3;
-                msg.s_id = getpid();
+                msg.s_id = pid;
                 strcpy(msg.msg_text, "EXIT");
                 msgsnd(q1, &msg, sizeof(msg) - sizeof(long), 0);
 
+				ex = 1;
+				running = 0;
                 break;
             }
 
-            if (sscanf(line, "%d %s %s", &input, loc, stat) != 3) {
-                pthread_mutex_lock(&lock);
+            if (sscanf(line, "%d %c %c", &input, loc, stat) != 3) {
                 printf("[ERROR]\n");
-                pthread_mutex_unlock(&lock);
                 continue;
             }
 
             if (input != id) {
-                pthread_mutex_lock(&lock);
                 printf("[ERROR]\n");
-                pthread_mutex_unlock(&lock);
                 continue;
             }
 
-            if (!(strcmp(loc,"A")==0 || strcmp(loc,"B")==0 ||
-                  strcmp(loc,"C")==0 || strcmp(loc,"D")==0) ||
-                !(strcmp(stat,"L")==0 || strcmp(stat,"H")==0)) {
-
-                pthread_mutex_lock(&lock);
+            if (!(loc == 'A' || loc == 'B' || loc == 'C' || loc == 'D') || !(stat == 'H' || stat == 'L')){
                 printf("[ERROR]\n");
-                pthread_mutex_unlock(&lock);
                 continue;
             }
 
             if (vcount == 0)
-                sprintf(d1, "%d %s %s", input, loc, stat);
+                sprintf(d1, "%d %c %c", input, loc, stat);
             else
-                sprintf(d2, "%d %s %s", input, loc, stat);
+                sprintf(d2, "%d %c %c", input, loc, stat);
 
             vcount++;
         }
@@ -135,13 +127,12 @@ int main() {
         if (vcount == 2) {
             sprintf(msg.msg_text, "%s | %s", d1, d2);
             msg.msg_type = 2;
-            msg.s_id = getpid();
+            msg.s_id = pid;
             msgsnd(q1, &msg, sizeof(msg) - sizeof(long), 0);
         }
     }
 	
     pthread_join(tid, NULL);
-
     pthread_mutex_destroy(&lock);
 
     printf("[SENSOR] Shutting down...\n");
